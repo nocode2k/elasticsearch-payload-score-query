179c789c254d58acc8a65648baa9054b27f2a4b9
NoCoDe <nocode2k@gmail.com>
2021-10-19 20:15:43 +0900

    sample code

--- src/main/java/nocode/elasticsearch/plugin/CustomPayloadScore.java
+++ src/main/java/nocode/elasticsearch/plugin/CustomPayloadScore.java
@@ -1,4 +1,16 @@
 package nocode.elasticsearch.plugin;
 
-public class CustomPayloadScore {
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.plugins.SearchPlugin;
+
+import java.util.Collections;
+import java.util.List;
+
+public class CustomPayloadScore extends Plugin implements SearchPlugin {
+    @Override
+    public List<QuerySpec<?>> getQueries() {
+        return Collections.singletonList(
+            new QuerySpec<>(CustomPayloadScoreBuilder.NAME, CustomPayloadScoreBuilder::new, CustomPayloadScoreBuilder::fromXContent)
+        );
+    }
 }

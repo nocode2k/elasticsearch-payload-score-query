ec48f55907dbcfd81db0668918912e9a9d5a93a5
박종성 <js.park@homeplus.co.kr>
2021-10-22 11:13:37 +0900

    rename class

--- src/main/java/nocode/elasticsearch/plugin/CustomPayloadScoreQueryPlugin.java
+++ src/main/java/nocode/elasticsearch/plugin/CustomPayloadScoreQueryPlugin.java
@@ -6,11 +6,11 @@ import org.elasticsearch.plugins.SearchPlugin;
 import java.util.Collections;
 import java.util.List;
 
-public class CustomPayloadScorePlugin extends Plugin implements SearchPlugin {
+public class CustomPayloadScoreQueryPlugin extends Plugin implements SearchPlugin {
     @Override
     public List<QuerySpec<?>> getQueries() {
         return Collections.singletonList(
-            new QuerySpec<>(CustomPayloadScoreBuilder.NAME, CustomPayloadScoreBuilder::new, CustomPayloadScoreBuilder::fromXContent)
+            new QuerySpec<>(CustomPayloadScoreQueryBuilder.NAME, CustomPayloadScoreQueryBuilder::new, CustomPayloadScoreQueryBuilder::fromXContent)
         );
     }
 }
